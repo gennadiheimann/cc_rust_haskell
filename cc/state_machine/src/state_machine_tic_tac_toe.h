a7f4cdb5995d73f20ac8d64935dce75b9fdf3e24
Gennadi Heimann <gennadiheimann@gmail.com>
2026-01-28 22:01:35 +0100

    feat: state machine for tic tac toe

---
+++ cc/state_machine/src/state_machine_tic_tac_toe.h
@@ -0,0 +1,9 @@
+#ifndef SRC_STATE_MACHINE_TIC_TAC_TOE_H
+#define SRC_STATE_MACHINE_TIC_TAC_TOE_H
+
+namespace state_machine_tic_tac_toe {
+class StateMachineTicTacToe{
+  
+};
+}
+#endif // SRC_STATE_MACHINE_TIC_TAC_TOE_H
