e8ee4fad428c1e34e575258615b290828bc3035a
Gennadi Heimann <bertrandt.heimann@extaccount.com>
2026-04-15 06:48:07 +0000

    feat: python loan comaprison
    add configuration package

---
+++ python/loan_comparison/configuration/config_reader.py
@@ -0,0 +1,21 @@
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+class ConfigReader:
+    def __init__(self, config_file_name):
+        with open(config_file_name, "rb") as f:
+            config = tomllib.load(f)
+    
+        # Algemein Bausparvertrag
+        self.bausparsumme = float(config["bsv"]["bs_sum"])
+        self.mindest_ansparung = float(config["bsv"]["minimum_saving_amount_decimal"])
+        self.abschlussgebuehr = float(config["bsv"]["acquisition_fee_decimal"])
+        # Ansparen
+        self.ansparen_guthabenzins = float(config["bsv"]["deposit_rates_decimal"])
+        self.ansparen_zeit = int(config["bsv"]["saving_time"])
+        self.ansparen_rate = float(config["bsv"]["saving_rate"])
+        # Darlehen
+        self.darlehen_zins = float(config["bsv"]["fixed_interest_rate_decimal"])
+        self.darlehen_rate = float(config["bsv"]["interest_prinipal_paymants_mounthly_decimal"])
