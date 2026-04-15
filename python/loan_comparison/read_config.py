3921b2ae4cbd53a0a366f2a0c5d2dfb64ac011f6
Gennadi Heimann <gennadiheimann@gmail.com>
2026-04-15 07:29:04 +0200

    feat: python loan comparison

--- python/loan_comparison/read_config.py
+++ python/loan_comparison/read_config.py
@@ -21,6 +21,8 @@ minsest_ansparung = float(config["bsv"]["minimum_saving_amount_decimal"])
 abschlus_gebuehr = float(config["bsv"]["acquisition_fee_decimal"])
 ansparzeit = int(config["bsv"]["saving_time"])
 ansparrate = float(config["bsv"]["saving_rate"])
+darlehen_zins = float(config["bsv"]["fixed_interest_rate_decimal"])
+darlehensrate = float(config["bsv"]["interest_prinipal_paymants_mounthly_decimal"])
 
 def calc_saving_amount(ansparzeit, ansparrate, bs_summe, guthaben_zins, abschlus_gebuehr):
     monatszins = guthaben_zins / 12
@@ -39,9 +41,11 @@ kapital = calc_saving_amount(
 
 # Finazierung für die Teilung
 rest_zu_finanzieren = (bs_summe * minsest_ansparung) - kapital
-
+print(f"########################################################")
+print(f"Ansparzeit")
 print(f"Kapital: {kapital:.2f}")
 print(f"Rest zu Finanzieren: {rest_zu_finanzieren:.2f}")
+print(f"########################################################")
 
 #=======================================================================================
 # Kredit
@@ -52,6 +56,9 @@ print(f"Rest zu Finanzieren: {rest_zu_finanzieren:.2f}")
 
 # Rest zu Finanzieren 
 
+print(f"########################################################")
+print(f"Rest 10 Jare finanzieren ")
+
 def berechne_kredit(kreditsumme, zinssatz_jahr, laufzeit_jahre):
     i = zinssatz_jahr / 100 / 12        # monatlicher Zinssatz
     n = laufzeit_jahre * 12             # Anzahl Monate
@@ -59,6 +66,7 @@ def berechne_kredit(kreditsumme, zinssatz_jahr, laufzeit_jahre):
     # Annuität (monatliche Rate)
     rate = kreditsumme * (i * (1 + i) ** n) / ((1 + i) ** n - 1)
 
+    
     restschuld = kreditsumme
 
     print(f"Monatliche Rate: {rate:.2f} €\n")
@@ -72,15 +80,78 @@ def berechne_kredit(kreditsumme, zinssatz_jahr, laufzeit_jahre):
         restschuld -= tilgung
         zinsen_gesamt_zeit += zinsen
 
-        print(f"{monat:5d} | {rate:7.2f} | {zinsen:7.2f} | {tilgung:8.2f} | {restschuld:10.2f}")
+        #print(f"{monat:5d} | {rate:7.2f} | {zinsen:7.2f} | {tilgung:8.2f} | {restschuld:10.2f}")
 
     print(f"Zinsen über gesamte Zeit: {zinsen_gesamt_zeit:.2f}")
+    return (rate, zinsen_gesamt_zeit)
 
-berechne_kredit(kreditsumme=rest_zu_finanzieren, zinssatz_jahr=6.0, laufzeit_jahre=int(ansparzeit / 12)) 
+(rest_finanzieren_rate, rest_gesamt_zinsen) = berechne_kredit(kreditsumme=rest_zu_finanzieren, zinssatz_jahr=6.0, laufzeit_jahre=int(ansparzeit / 12)) 
+
+print(f"########################################################")
 
 #==============================================================================
 # Darlehenphase
 #==============================================================================
+
+print(f"########################################################")
+import math
+
+def kredit_plan(kreditsumme, zinssatz_jahr, rate):
+    i = zinssatz_jahr / 100 / 12  # monatlicher Zinssatz
+
+    # Laufzeit berechnen
+    n = math.log(rate / (rate - kreditsumme * i)) / math.log(1 + i)
+    monate = math.ceil(n)
+
+    restschuld = kreditsumme
+
+    print(f"Laufzeit: {monate} Monate (~{monate/12:.1f} Jahre)\n")
+    #print("Monat | Rate   | Zinsen | Tilgung | Restschuld")
+    #print("--------------------------------------------------")
+
+    zinsen_ueber_gesamte_zeit = 0.0
+    
+    for monat in range(1, monate + 1):
+        zinsen = restschuld * i
+        zinsen_ueber_gesamte_zeit += zinsen
+        tilgung = rate - zinsen
+        restschuld -= tilgung
+
+        # Rundungsfehler am Ende korrigieren
+        if restschuld < 0:
+            tilgung += restschuld
+            restschuld = 0
+
+        #print(f"{monat:5d} | {rate:7.2f} | {zinsen:7.2f} | {tilgung:8.2f} | {restschuld:10.2f}")
+
+        if restschuld <= 0:
+            break
+        
+    print(f"Zinsen ueber gesamte Lufzeit: {zinsen_ueber_gesamte_zeit:.2f}")
+    return zinsen_ueber_gesamte_zeit
+
+
+# Beispiel
+darlehen_gesamt_zinsen = kredit_plan(
+    kreditsumme=bs_summe - (bs_summe * minsest_ansparung),
+    zinssatz_jahr= darlehen_zins,     
+    rate=bs_summe * darlehensrate
+)
+
+print(f"########################################################")
+
+zinsen_belastungfrei_darlehen = bs_summe * 0.045 * ansparzeit / 12
+print(f"Belastung Tilgungsfreier Darlehen: {zinsen_belastungfrei_darlehen:.2f}")
+belastung_tilgungsfrei_darlehen = bs_summe * 0.045 / 12
+print(f"Belastung monatlich: {belastung_tilgungsfrei_darlehen:.2f}")
+belastung_0_10 = belastung_tilgungsfrei_darlehen + ansparrate
+print(f"Belastung 0-10 Jahre: {belastung_0_10:.2f}")
+belastung_11_20 = rest_finanzieren_rate + (bs_summe*darlehensrate)
+print(f"Belastung 11-20 Jahre: {belastung_11_20:.2f}")
+print(f"Belastung 11-30 Jahre: {bs_summe*darlehensrate}")
+gesamt_zinsen = zinsen_belastungfrei_darlehen + rest_gesamt_zinsen + darlehen_gesamt_zinsen
+print(f"Geasmt Zinsen: {gesamt_zinsen:.2f}")
+
 
 #========================================================================================
 # export
