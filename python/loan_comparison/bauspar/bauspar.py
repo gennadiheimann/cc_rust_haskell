39a66e5e6f1a0363fa9223db4fa55299327aa2c6
Gennadi Heimann <bertrandt.heimann@extaccount.com>
2026-04-17 09:42:25 +0000

    feat: python loan_comparison correct Ansparen

--- python/loan_comparison/bauspar/bauspar.py
+++ python/loan_comparison/bauspar/bauspar.py
@@ -5,8 +5,14 @@ class Bauspar:
         self.config = config
     
     def calculate_saving_amount(self):
-        monthly_interest = self.config.ansparen_guthabenzins / 12
-        total_saving_amount = 0.0
+        monthly_rate = self.config.ansparen_guthabenzins
+        day_factor = 365/360
+
+        total_saving_amount = 0 -  (self.config.bausparsumme * self.config.abschlussgebuehr)
+
         for _ in range(self.config.ansparen_zeit):
-            total_saving_amount = total_saving_amount * (1 + monthly_interest) + self.config.ansparen_rate
-        return total_saving_amount - (self.config.bausparsumme * self.config.abschlussgebuehr)
+            total_saving_amount += self.config.ansparen_rate
+            if total_saving_amount > 0:
+                interest = total_saving_amount * monthly_rate * day_factor / 12
+                total_saving_amount += interest
+        return total_saving_amount
